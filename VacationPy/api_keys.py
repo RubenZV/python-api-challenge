1ead2fc7f4b20917de1a10486668cd085edfebd1
prophet60091 <r.zambrano06@hotmail.com>
2020-03-29 18:28:13 -0600

    This is my APIs challenge
    Corona free.

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bd2b6645aae57f07b41a26525d991ec2"
+
+# Google API Key
+g_key = "AIzaSyBf0MiRCMdXJa1nD7F4eV5WDZbXV-aJU0Q"
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bd2b6645aae57f07b41a26525d991ec2"
+
+# Google API Key
+g_key = "AIzaSyDYPsDm1QYzODKymipzdFsf61mfYHLXXRo"
